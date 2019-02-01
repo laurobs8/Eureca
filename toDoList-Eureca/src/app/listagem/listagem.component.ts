69e275b45bbdf323cc49036f2e5e55ea084d6a7a
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-01 04:03:53 -0200

    Criacao do componente que mostrará os dados

---
+++ toDoList-Eureca/src/app/listagem/listagem.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TarefaService } from '../shared/tarefa.service';
+import { TarefaDataService } from '../shared/tarefa-data.service';
+import { Tarefa } from '../shared/tarefa';
+
+
+@Component({
+  selector: 'app-listagem',
+  templateUrl: './listagem.component.html',
+  styleUrls: ['./listagem.component.css']
+})
+export class ListagemComponent implements OnInit {
+
+  tarefas: Observable<any>;
+
+  constructor(private tarefaService: TarefaService, private tarefaDataService: TarefaDataService) { }
+
+  ngOnInit() {
+    this.tarefas = this.tarefaService.todos();
+  }
+
+  // delete(key: string) {
+  //   this.tarefaService.delete(key); // TODO
+  // }
+
+  editar(tarefa: Tarefa, key: string) {
+    this.tarefaDataService.mudarTarefa(tarefa, key);
+  }
+}
