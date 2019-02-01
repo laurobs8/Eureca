4e03ad0be4b06824f30a96bb9ec8e880e1f13449
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-01 04:06:31 -0200

    Declaracoes dos componentes criados

--- toDoList-Eureca/src/app/app.module.ts
+++ toDoList-Eureca/src/app/app.module.ts
@@ -3,17 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { ListagemComponent } from './listagem/listagem.component';
+import { EditarComponent } from './editar/editar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    ListagemComponent,
+    EditarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
