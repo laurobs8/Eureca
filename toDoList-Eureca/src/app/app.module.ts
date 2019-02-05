fda65bb934f4c416ad677d4afd852550e54f2b6c
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-05 11:32:05 -0200

    Exclusão de componentes e classes sem uso

--- toDoList-Eureca/src/app/app.module.ts
+++ toDoList-Eureca/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
@@ -14,7 +13,6 @@ import { EditarComponent } from './editar/editar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListComponent,
     ListagemComponent,
     EditarComponent,
   ],
