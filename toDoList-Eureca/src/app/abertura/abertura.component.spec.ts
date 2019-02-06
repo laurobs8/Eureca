b1a3d2cbb9774f0348aa3a3e96dd56096a4c16cb
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-06 07:44:22 -0200

    Componente da tela de apresentacao

---
+++ toDoList-Eureca/src/app/abertura/abertura.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AberturaComponent } from './abertura.component';
+
+describe('AberturaComponent', () => {
+  let component: AberturaComponent;
+  let fixture: ComponentFixture<AberturaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AberturaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AberturaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
